bac3600fb0226b30330796013088ff8a4328d96b
nabsha@gmail.com <nabsha@gmail.com@3447bb64-e9f0-989c-09f5-d66d62bebc40>
2012-08-20 17:32:28 +0000

    - Refactoring structure to accomodate new chart - Dynamic Live charts added - Live start/stop feature added - Client UI seperated from dashboard business logic

---
+++  open-dashboard --username nabsha@gmail.com/src/main/java/com/odb/view/dashboard/client/ClientCommons.java
@@ -0,0 +1,9 @@
+package com.odb.view.dashboard.client;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+public class ClientCommons {
+	/** The date format. */
+	public static DateTimeFormat dateFormat = DateTimeFormat.getFormat("dd/MM/yyyy h:m:s a");
+
+}
